5e225bc0dbf1264c1f43fd44ec0c2a713c10a8c4
pbouillon <pro.pierre.bouillon@protonmail.com>
2019-05-04 15:06:49 +0200

    add core

---
+++ Core/UrlShortener.Api/Startup.cs
@@ -0,0 +1,80 @@
+﻿using Blog.Common.Contracts.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace UrlShortener.Api
+{
+    public class Startup
+    {
+        private ProjectDto _projectMeta;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _projectMeta = new ProjectDto();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Load project's definition
+            Configuration.GetSection("Project").Bind(_projectMeta);
+            Configuration.GetSection("Project:Contact").Bind(_projectMeta.Contact);
+            Configuration.GetSection("Project:License").Bind(_projectMeta.License);
+
+            // Populate swagger UI
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(_projectMeta.ApiVersion, new Info
+                {
+                    Version = _projectMeta.Version,
+                    Title = _projectMeta.Title,
+                    Description = _projectMeta.Description,
+                    TermsOfService = _projectMeta.TermsOfService,
+                    Contact = new Contact
+                    {
+                        Name = _projectMeta.Contact.Name,
+                        Email = _projectMeta.Contact.Email,
+                        Url = _projectMeta.Contact.Url
+                    },
+                    License = new License
+                    {
+                        Name = _projectMeta.License.Name,
+                        Url = _projectMeta.License.Url
+                    }
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+
+            // Enable swagger middleware
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", _projectMeta.Title);
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
