4776f888768c61938d3f437e21e16b2f7e9a2161
pbouillon <pro.pierre.bouillon@protonmail.com>
2019-05-04 15:22:52 +0200

    add custom controller

---
+++ Core/UrlShortener.Api/Controllers/LongUrlController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.Common.Contracts.Url;
+
+namespace UrlShortener.Api.Controllers
+{
+    [Route("url/long")]
+    [ApiController]
+    [Produces("application/json")]
+    public class LongUrlController : ControllerBase
+    {
+        [HttpGet("{longUrl}")]
+        public ActionResult<UrlDto> GetShortUrlFor([FromRoute] string longUrl)
+        {
+            return Ok(new UrlDto
+            {
+                LongUrl = longUrl,
+                ShortUrl = "short url"
+            });
+        }
+
+        [HttpPost("{longUrl}")]
+        public ActionResult<UrlDto> ShortenNewUrl([FromRoute] string longUrl)
+        {
+            return Ok(new UrlDto
+            {
+                LongUrl = longUrl,
+                ShortUrl = "short url"
+            });
+        }
+    }
+}
---
+++ Core/UrlShortener.Common/Contracts/Url/UrlDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Common.Contracts.Url
+{
+    public class UrlDto
+    {
+        public string LongUrl { get; set; }
+
+        public string ShortUrl { get; set; }
+    }
+}
