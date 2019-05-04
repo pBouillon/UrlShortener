b34daa814db9681c8b64e92d7e44bb997f1b770c
pbouillon <pro.pierre.bouillon@protonmail.com>
2019-05-04 18:54:42 +0200

    add custom exception

---
+++ Core/UrlShortener.Common/Constants/Url/ExceptionMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Service.Url.Exceptions
+{
+    public class ExceptionMessages
+    {
+        public const string BadUrlProvided = "The provided url shouldn't be empty.";
+    }
+}
