47d0ef5c3cbb9c5c8250f9bd896d3c1c78147220
pbouillon <pro.pierre.bouillon@protonmail.com>
2019-05-04 15:06:57 +0200

    add contracts

---
+++ Core/UrlShortener.Common/Contracts/Configuration/LicenseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Common.Contracts.Configuration
+{
+    public class LicenseDto
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ Core/UrlShortener.Common/Contracts/Configuration/ProjectDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Common.Contracts.Configuration
+{
+    public class ProjectDto
+    {
+        public string ApiVersion { get; set; }
+        public ContactDto Contact { get; set; }
+        public string Description { get; set; }
+        public LicenseDto License { get; set; }
+        public string TermsOfService { get; set; }
+        public string Title { get; set; }
+        public string Version { get; set; }
+    }
+}
