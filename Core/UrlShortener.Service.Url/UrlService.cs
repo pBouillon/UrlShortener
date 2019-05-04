30200b593e91fc15b3db2496a6051b4f3357a24c
pbouillon <pro.pierre.bouillon@protonmail.com>
2019-05-04 18:55:10 +0200

    add service skeleton

--- Core/UrlShortener.Service.Url/UrlService.cs
+++ Core/UrlShortener.Service.Url/UrlService.cs
@@ -1,4 +1,7 @@
-﻿using UrlShortener.Common.Contracts.Url;
+﻿using System;
+using System.Security.Cryptography;
+using UrlShortener.Common.Contracts.Url;
+using UrlShortener.Service.Url.Exceptions;
 using UrlShortener.Service.Url.Interfaces;
 
 namespace UrlShortener.Service.Url
@@ -6,12 +9,43 @@ namespace UrlShortener.Service.Url
     public class UrlService : IUrlService
     {
         /// <summary>
+        /// Default constructor for the UrlService
+        /// </summary>
+        public UrlService() { }
+
+        /// <summary>
+        /// Given a string, return a random sequence of a specific length
+        /// </summary>
+        /// <param name="toHash"></param>
+        /// <see cref="UrlGeneration.GeneratedSequenceLength"/>
+        /// <returns></returns>
+        private string GetShortenString(string toShorten)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// TODO doc
+        /// </summary>
+        /// <param name="toHash"></param>
+        /// <returns></returns>
+        private string GetMd5Hash(string toHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
         /// Given a short url, fetch the long one
         /// </summary>
         /// <param name="shortUrl">Short url provided</param>
         /// <returns>The long url in a `UrlDto`</returns>
         public UrlDto GetLongUrlFor(string shortUrl)
         {
+            if (string.IsNullOrEmpty(shortUrl))
+            {
+                throw new InvalidRequestException(ExceptionMessages.BadUrlProvided);
+            }
+
             return new UrlDto
             {
                 LongUrl = "Associated long url",
@@ -26,10 +60,15 @@ namespace UrlShortener.Service.Url
         /// <returns>The short url in a `UrlDto`</returns>
         public UrlDto GetShortUrlFor(string longUrl)
         {
+            if (string.IsNullOrEmpty(longUrl))
+            {
+                throw new InvalidRequestException(ExceptionMessages.BadUrlProvided);
+            }
+
             return new UrlDto
             {
                 LongUrl = longUrl,
-                ShortUrl = "Associated short url"
+                ShortUrl = GetShortenString(longUrl)
             };
         }
     }
